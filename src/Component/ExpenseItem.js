20f08e6618105bac5adde774a3459302448e3959
SrinivasaKamathR <kamathsri1997@gmail.com>
2022-12-20 13:05:39 +0530

    first-commit

---
+++ src/Component/ExpenseItem.js
@@ -0,0 +1,28 @@
+export default function ExpenseItem() {
+  return (
+    <div>
+      {/* <table style={{ border: "2px solid black" }}>
+        <thead>
+          <tr>
+            <th>Expense Items</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Food Rs 10</td>
+          </tr>
+          <tr>
+            <td>Petrol Rs 100</td>
+          </tr>
+          <tr>
+            <td>Movies Rs 200</td>
+          </tr>
+        </tbody>
+      </table> */}
+      <h2>Expense Items</h2>
+      <p>Food Rs 10</p>
+      <p>Petrol Rs 100</p>
+      <p>Movies Rs 200</p>
+    </div>
+  );
+}
