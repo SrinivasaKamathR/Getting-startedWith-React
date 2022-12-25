c5aee356165c10e44dacfd913f4c03ab0467b9f4
SrinivasaKamathR <kamathsri1997@gmail.com>
2022-12-25 13:10:14 +0530

    seventh-commit-form-submitting

--- src/Component/NewExpense/ExpenseForm.js
+++ src/Component/NewExpense/ExpenseForm.js
@@ -6,17 +6,38 @@ export const ExpenseForm = () => {
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
 
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
+
   const titleChangeHandler = (e) => {
     setEnteredTitle(e.target.value);
+    // setUserInput({ ...userInput, enteredTitle: e.target.value });
+    // setUserInput = (prevState) => {
+    //   return { ...prevState, enteredTitle: e.target.value };
+    // };
   };
   const amountChangeHandler = (e) => {
     setEnteredAmount(e.target.value);
+    // setUserInput({ ...userInput, enteredAmount: e.target.value });
   };
   const dateChangeHandler = (e) => {
     setEnteredDate(e.target.value);
+    // setUserInput({ ...userInput, enteredAmount: e.target.value });
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const expenseData = {
+      title: enteredtitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
   };
   return (
-    <form action="">
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
